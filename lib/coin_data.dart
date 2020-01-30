8a21ad4caeb2b00c3648a665850fdf53249fc3bf
Mariobahaa <mariobahaa59s@gmail.com>
2020-01-30 03:06:32 +0200

    bit coin ticker up and running with three crypto currencies and a lot of currencies, ? missing

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 const List<String> currenciesList = [
@@ -36,17 +37,21 @@ const bitcoinAverageURL =
 
 class CoinData {
   Future getCoinData(String selectedCurrency) async {
-    //TODO 4: Use a for loop here to loop through the cryptoList and request the data for each of them in turn.
-    //TODO 5: Return a Map of the results instead of a single value.
-    String requestURL = '$bitcoinAverageURL/BTC$selectedCurrency';
-    http.Response response = await http.get(requestURL);
-    if (response.statusCode == 200) {
-      var decodedData = jsonDecode(response.body);
-      var lastPrice = decodedData['last'];
-      return lastPrice;
-    } else {
-      print(response.statusCode);
-      throw 'Problem with the get request';
+    // Use a for loop here to loop through the cryptoList and request the data for each of them in turn.
+    // Return a Map of the results instead of a single value.
+    Map<String, double> results = Map<String, double>();
+    for (String cryptoCurrency in cryptoList) {
+      String requestURL = '$bitcoinAverageURL/$cryptoCurrency$selectedCurrency';
+      http.Response response = await http.get(requestURL);
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        var lastPrice = decodedData['last'];
+        results[cryptoCurrency] = lastPrice;
+      } else {
+        print(response.statusCode);
+        throw 'Problem with the get request';
+      }
     }
+    return results;
   }
 }
